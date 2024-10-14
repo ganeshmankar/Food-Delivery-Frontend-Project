96d7d28bfc31f086cb3baa489954add65701850f
Ganesh Mankar <ganeshnmankar28@gmail.com>
2024-10-14 13:49:51 +0530

    Front end project
    Signed-off-by: Ganesh Mankar <ganeshnmankar28@gmail.com>

---
+++ frontend_project/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Craving delicious food delivered straight to your door? Look no further! Our app connects you with a wide variety of restaurants offering mouth-watering dishes, all just a tap away. Whether you're in the mood for a quick snack, a hearty meal, or something sweet, we've got you covered with fast, reliable delivery. Satisfy your hunger with ease, convenience, and a great selection, all in one place!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
